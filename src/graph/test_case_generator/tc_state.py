f1b1f516ef4b19b0c60a11844e9c45a97dc40f40
kpsajesh <sajeshkumarkp@gmail.com>
2025-10-05 10:50:14 +0100

    Added up to state file

---
+++ src/graph/test_case_generator/tc_state.py
@@ -0,0 +1,35 @@
+# This file is to store the state variables of testcase generator pipeline
+
+"""
+Test Case Generator (LangGraph)
+---------------------------------------
+This file defines the State class for our pipeline.
+The State represents the "data backpack" that moves
+through each node of the graph.
+"""
+
+from typing import List, Dict, TypedDict, Optional
+
+
+class TestCaseState(TypedDict, total=False): 
+    # Total=False means all state variables are optional, ie the as the flow progresses the variables are filled one by one
+    # Means in the begining only requirements will be there, then tests will be added, then testrail_case_ids and so on.
+    """
+    State for the Test Case Generator pipeline.
+    Fields here are carried across all nodes in the graph.
+    """
+
+    # Input: requirements text 
+    requirements: str
+
+    # Output: list of generated test cases
+    tests: List[str]
+
+    # Output: TestRail case IDs after pushing
+    testrail_case_ids: List[str]
+
+    # Execution report (optional, future use)
+    execution_report: Dict
+
+    # Errors (if any) collected during pipeline
+    errors: List[str]
